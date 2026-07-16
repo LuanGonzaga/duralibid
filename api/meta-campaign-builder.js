316b35e3cb6c3048cf11fa6c00afca6b27dfd382
Luan Gonzaga <luangonzaga1233@gmail.com>
2026-07-16 14:58:09 -0300

    Send budget sharing flag on Meta campaign

--- api/meta-campaign-builder.js
+++ api/meta-campaign-builder.js
@@ -215,6 +215,7 @@ async function createCampaign({ name, sourceCampaignId, dailyBudget = 20, kit = 
     objective: 'OUTCOME_LEADS',
     status: 'PAUSED',
     buying_type: 'AUCTION',
+    is_adset_budget_sharing_enabled: 'false',
     special_ad_categories: JSON.stringify([]),
   }, 'POST');
 
@@ -226,7 +227,6 @@ async function createCampaign({ name, sourceCampaignId, dailyBudget = 20, kit = 
       daily_budget: String(Math.max(1, Number(dailyBudget)) * 100),
       billing_event: 'IMPRESSIONS',
       optimization_goal: 'OFFSITE_CONVERSIONS',
-      is_adset_budget_sharing_enabled: 'false',
       bid_strategy: 'LOWEST_COST_WITHOUT_CAP',
       destination_type: 'WEBSITE',
       promoted_object: JSON.stringify({
